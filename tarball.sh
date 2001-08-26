a2828be1be26fc27de7a33766348f650727963e9
drobbins <drobbins@ac592a22-f3fe-0310-977e-98394eae9e84>
2001-08-26 04:33:43 +0000

    Portage has been added to gentoo-src

---
+++ tarball.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export PKG="portage"
+export TMP="/tmp"
+export V="1.6"
+export DEST="${TMP}/${PKG}-${V}"
+rm -rf ${DEST}
+install -d -m0755 ${DEST}
+for x in bin cnf man pym src 
+do
+	cp -ax $x ${DEST}
+	rm -rf ${DEST}/${x}/CVS
+done
+chown -R root.root ${DEST}
+cd $TMP
+tar cjvf ${TMP}/${PKG}-${V}.tar.bz2 ${PKG}-${V}
