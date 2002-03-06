261d8b9fb5d7239061080bb509ecdadde3281d1c
gbevin <gbevin@ac592a22-f3fe-0310-977e-98394eae9e84>
2002-03-06 09:51:02 +0000

    changed copyright

--- src/sandbox/libsandbox.c
+++ src/sandbox/libsandbox.c
@@ -8,9 +8,9 @@
 **
 **  it's very important that the --enable-static-link option is NOT specified
 **	
-**	Copyright (C) 2001 The Leaf, http://www.theleaf.be
-**  Distributed under the terms of the GNU General Public License, v2 or later 
-**	Author : Geert Bevin <gbevin@theleaf.be>
+**	Copyright (C) 2001 Geert Bevin, Uwyn, http://www.uwyn.com
+**	Distributed under the terms of the GNU General Public License, v2 or later 
+**	Author : Geert Bevin <gbevin@uwyn.com>
 **  $Header$
 */
 
