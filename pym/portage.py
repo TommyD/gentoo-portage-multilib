6b34ad52a061fdd434a0a95ac150ce0f07cd73b1
carpaski <carpaski@ac592a22-f3fe-0310-977e-98394eae9e84>
2002-10-25 02:17:01 +0000

    Added section on Masks to emerge.1 Update version on portage.py to 2.0.42 :) Added more info to --version and 'emerge info'

--- pym/portage.py
+++ pym/portage.py
@@ -2,7 +2,7 @@
 # Copyright 1998-2002 Daniel Robbins, Gentoo Technologies, Inc.
 # Distributed under the GNU Public License v2
 
-VERSION="2.0.41-pre"
+VERSION="2.0.42"
 
 from stat import *
 from commands import *
