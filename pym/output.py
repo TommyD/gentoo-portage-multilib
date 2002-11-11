b22a500d4a572d7a3b7272ef9bb0cb47d5a6d20e
carpaski <carpaski@ac592a22-f3fe-0310-977e-98394eae9e84>
2002-11-11 19:29:24 +0000

    mild touchups to the 2.0.44 release

--- pym/output.py
+++ pym/output.py
@@ -4,9 +4,16 @@
 codes={}
 codes["reset"]="\x1b[0m"
 codes["bold"]="\x1b[01m"
+codes["teal"]="\x1b[36;03m"
 codes["turquoise"]="\x1b[36;01m"
-codes["green"]="\x1b[32;01m"
+codes["fuscia"]="\x1b[35;01m"
+codes["purple"]="\x1b[35;03m"
+codes["blue"]="\x1b[34;01m"
+codes["darkblue"]="\x1b[34;03m"
 codes["yellow"]="\x1b[33;01m"
+codes["brown"]="\x1b[33;03m"
+codes["green"]="\x1b[32;01m"
+codes["darkgreen"]="\x1b[32;03m"
 codes["red"]="\x1b[31;01m"
 
 def nocolor():
@@ -23,11 +30,23 @@ def ctext(color,text):
 def bold(text):
 	return codes["bold"]+text+codes["reset"]
 
+def teal(text):
+	return codes["teal"]+text+codes["reset"]
+
 def turquoise(text):
 	return codes["turquoise"]+text+codes["reset"]
 
 def green(text):
+	return codes["blue"]+text+codes["reset"]
+
+def darkgreen(text):
+	return codes["darkblue"]+text+codes["reset"]
+
+def green(text):
 	return codes["green"]+text+codes["reset"]
+
+def darkgreen(text):
+	return codes["darkgreen"]+text+codes["reset"]
 
 def white(text):
 	return codes["bold"]+text+codes["reset"]
