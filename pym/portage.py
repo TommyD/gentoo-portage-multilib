36e5dc44f3ce5602736d2652aef5a016ee7caded
carpaski <carpaski@ac592a22-f3fe-0310-977e-98394eae9e84>
2003-02-07 06:09:20 +0000

    I didn't think that was funny... One line, not 6. movefile() should be fine now.

--- pym/portage.py
+++ pym/portage.py
@@ -1455,22 +1455,13 @@ def movefile(src,dest,newmtime=None,sstat=None):
 		if S_ISLNK(dstat[ST_MODE]):
 			try:
 				os.unlink(dest)
+				destexists=0
 			except Exception, e:
 				pass
 
 	if S_ISLNK(sstat[ST_MODE]):
 		try:
 			target=os.readlink(src)
-
-			# Well just cuz it didn't exist up there doesn't
-			# mean it doesn't exist now, silly.
-			destexists=1
-			try:
-				dstat=os.lstat(dest)
-			except:
-				dstat=os.lstat(os.path.dirname(dest))
-				destexists=0
-
 			if destexists and not S_ISDIR(dstat[ST_MODE]):
 				os.unlink(dest)
 			os.symlink(target,dest)
