a4426c7485bf6064affde820d9e5c3e346c345a4
carpaski <carpaski@ac592a22-f3fe-0310-977e-98394eae9e84>
2003-11-10 17:03:53 +0000

    New tool to check digests and SRC_URIs.

---
+++ bin/md5check.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python -O
+
+import os,sys,string
+os.environ["PORTAGE_CALLER"]="mirror"
+os.environ["FEATURES"]="mirror cvs"
+sys.path = ["/usr/lib/portage/pym"]+sys.path
+
+import portage
+from threading import *
+from output import red,green,blue,bold
+from random import shuffle
+from time import sleep
+
+
+def cstrip(mystr,mychars):
+	newstr = ""
+	for x in mystr:
+		if x not in mychars:
+			newstr += x
+	return newstr
+
+md5_list = {}
+bn_list  = []
+col_list = []
+
+hugelist = []
+for mycp in portage.db["/"]["porttree"].dbapi.cp_all():
+	hugelist += portage.db["/"]["porttree"].dbapi.cp_list(mycp)
+hugelist.sort()
+
+for mycpv in hugelist:
+	pv = string.split(mycpv, "/")[-1]
+
+	newuri = portage.db["/"]["porttree"].dbapi.aux_get(mycpv,["SRC_URI"])[0]
+	newuri = string.split(newuri)
+
+	digestpath = portage.db["/"]["porttree"].dbapi.findname(mycpv)
+	digestpath = os.path.dirname(digestpath)+"/files/digest-"+pv
+	md5sums    = portage.digestParseFile(digestpath)
+	
+	if md5sums == None:
+		portage.writemsg("Missing digest: %s\n" % mycpv)
+		md5sums = {}
+
+	for x in newuri:
+		if not x:
+			continue
+		if (x in [")","(",":","||"]) or (x[-1] == "?"):
+			# ignore it. :)
+			continue
+		x = cstrip(x,"()|?")
+		if not x:
+			continue
+
+		mybn = os.path.basename(x)
+		if mybn not in bn_list:
+			bn_list += [mybn]
+		
+		if mybn not in md5sums.keys():
+			portage.writemsg("Missing md5sum: %s in %s\n" % (mybn, mycpv))
+		else:
+			if mybn in md5_list.keys():
+				if (md5_list[mybn][0] != md5sums[mybn][0]) or \
+				   (md5_list[mybn][1] != md5sums[mybn][1]):
+					portage.writemsg("Colliding md5: %s of %s and %s\n" % (mybn,mycpv,md5_list[mybn][2]))
+					col_list += [mybn]
+				else:
+					md5_list[mybn][2] += ","+mycpv
+			else:
+				md5_list[mybn] = md5sums[mybn]+[mycpv]
+			del md5sums[mybn]
+		
+	for x in md5sums.keys():
+		portage.writemsg("Extra md5sum: %s in %s\n" % (mybn, mycpv))
+
+print col_list
+print
+print str(len(md5_list.keys()))+" unique distfile md5s."
+print str(len(bn_list))+" unique distfile names."
