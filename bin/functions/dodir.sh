d962c2785e57c3fdeb4d58d74e0a3295bb59ece6
carpaski <carpaski@ac592a22-f3fe-0310-977e-98394eae9e84>
2004-03-20 23:48:43 +0000

    This would make the environment huge... so we're going to forego having them ever in the environment.

--- bin/functions/dodir.sh
+++
@@ -1,9 +0,0 @@
-# Copyright 1999-2003 Gentoo Technologies, Inc.
-# Distributed under the terms of the GNU General Public License v2
-# $Header$
-
-dodir() {
-	for x in "$@" ; do
-		install -d ${DIROPTIONS} "${D}${x}" || die
-	done
-}
--- bin/functions/dohtml.sh
+++
@@ -1,7 +0,0 @@
-# Copyright 1999-2003 Gentoo Technologies, Inc.
-# Distributed under the terms of the GNU General Public License v2
-# $Header$
-
-dohtml() {
-	dohtml.py "$@" || die "dohtml filed"
-}
