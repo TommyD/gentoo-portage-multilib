83dd1763bf15edd36fc035b41d3bf86564c4db79
ferringb <ferringb@ac592a22-f3fe-0310-977e-98394eae9e84>
2004-11-11 00:16:22 +0000

    +1 again.

--- bin/ebuild-default-functions.sh
+++ bin/ebuild-default-functions.sh
@@ -385,7 +385,7 @@ dyn_test() {
 function stat_perms() {
 	local f
 	f=$(stat -c '%f' "$1")
-	f=$(printf %o 0x$f)
+	f=$(printf "%o" 0x$f)
 	f="${f:${#f}-4}"
 	echo $f
 }
@@ -460,14 +460,14 @@ dyn_install() {
 			ewarn "file $file was installed with group portage!"
 			s=$(stat_perms "$file")
 			chgrp wheel "$file"
-			chmod "%s" "$file"
+			chmod "$s" "$file"
 		done
 	else
 		find "${D}/" -group portage -print | while read file; do
 			ewarn "file $file was installed with group portage!"
 			s=$(stat_perms "$file")
 			chgrp root "$file"
-			chmod "%s" "$file"
+			chmod "$s" "$file"
 		done
 	fi
 
