58a279877f53ea169f707dd79cbfadc0b6459591
genone <genone@ac592a22-f3fe-0310-977e-98394eae9e84>
2004-11-18 01:25:43 +0000

    fixing versioning regexp

--- pym/portage_versions.py
+++ pym/portage_versions.py
@@ -1,6 +1,6 @@
 import re,string
 
-ver_regexp = re.compile("^(cvs\\.)?(\\d+)((\\.\\d+)*)([a-zA-Z]?)((_(pre|p|beta|alpha|rc)\\d*)*)(-r(\\d+))?$")
+ver_regexp = re.compile("^(cvs\\.)?(\\d+)((\\.\\d+)*)([a-z]?)((_(pre|p|beta|alpha|rc)\\d*)*)(-r(\\d+))?$")
 suffix_regexp = re.compile("^(alpha|beta|rc|pre|p)(\\d*)$")
 suffix_value = {"pre": -2, "p": 0, "alpha": -4, "beta": -3, "rc": -1}
 
