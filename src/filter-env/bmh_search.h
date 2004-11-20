0862e77f5dc664f5c41464d70b4bc5d6ba2136cd
ferringb <ferringb@ac592a22-f3fe-0310-977e-98394eae9e84>
2004-11-20 10:14:51 +0000

    hellllllllooooooo filter-env. simple binary to walk the structure of a bash env/prog, and optionally mask portions of the it via specified opts fex, to read from stdin and outputing to stdout, and filtering func1,func2 and var1 filter-env -f func1,func2 -v var1 alternativelly, from file, and getting var1,var2 filter-env -i file -v var1,var2 etc.  Always outputs to stdout, although that might change.  Tested it against a full vdb environment dump (all files)- 100+ envs merged, walked it fine. That said, if you hit a bug, it's quite likely a HERE related structure- cat <<BLAH asdf asdf BLAH fex. If you hit oddities, use -d to turn on debugging info.  currently 2 levels of it- first is func matching and env matching, second level enable debug info for HERE related structure matching. No deps aside from regex supplied by glibc.

---
+++ src/filter-env/bmh_search.h
@@ -0,0 +1,7 @@
+/* raided from http://www.dcc.uchile.cl/~rbaeza/handbook/algs/7/713b.srch.c.html
+aparently distributed by Addison-Wesley Publishing Co. Inc, http://aw.com/
+*/
+
+char *bmh_search(pat, text, n );
+//char *pat, *text;
+//int n;
