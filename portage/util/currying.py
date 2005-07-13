10f9c1e6c97393b9378eed1c6077a7e29249b5a0
ferringb <ferringb@ac592a22-f3fe-0310-977e-98394eae9e84>
2005-07-13 07:22:08 +0000

    header adjustment

--- portage/util/currying.py
+++ portage/util/currying.py
@@ -1,5 +1,7 @@
+# Copyright: 2005 Gentoo Foundation
 # Author(s): Lifted from python cookbook, Scott David Daniels, Ben Wolfson, Nick Perkins, Alex Martelli for curry routine.
 # License: GPL2
+# $Header$
 
 def curry(*args, **kargs):
 	def callit(*moreargs, **morekargs):
