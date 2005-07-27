a638202a18874650fbad8c4e2d7bde72e4ce6b78
ferringb <ferringb@ac592a22-f3fe-0310-977e-98394eae9e84>
2005-07-27 02:20:13 +0000

    threw out some unused vars

--- portage/const.py
+++ portage/const.py
@@ -12,16 +12,15 @@ cvs_id_string="$Id$"[5:-2]
 PRIVATE_PATH            = "/var/lib/portage"
 
 USER_CONFIG_PATH        = "/etc/portage"
-#CUSTOM_PROFILE_PATH     = USER_CONFIG_PATH+"/profile"
 
-#PORTAGE_BASE_PATH       = "/usr/lib/portage"
+
 try:
 	import portage_custom_path
 
 except (ImportError, AttributeError):
 	portage_custom_path = None
 	print "warning, can't find portage_custom_path.  which means no custom PORTAGE_BASE_PATH"
-	print "so... that means you're getting /home/bharring/new/ , which quite likely isn't what you want"
+	print "so... that means you're getting /usr/lib/portage/ as a base , which quite likely isn't what you want"
 
 PORTAGE_BASE_PATH	= getattr(portage_custom_path, "PORTAGE_BASE_PATH", "/usr/lib/portage/")
 PORTAGE_BIN_PATH	= getattr(portage_custom_path, "PORTAGE_BIN_PATH", PORTAGE_BASE_PATH+"/bin")
@@ -51,18 +50,9 @@ WORLD_FILE              = PRIVATE_PATH+"/world"
 #MAKE_CONF_FILE          = "/etc/make.conf"
 #MAKE_DEFAULTS_FILE      = PROFILE_PATH + "/make.defaults"
 
-#DEPRECATED_PROFILE_FILE = PROFILE_PATH+"/deprecated"
-#USER_VIRTUALS_FILE      = USER_CONFIG_PATH+"/virtuals"
-#EBUILD_SH_ENV_FILE      = USER_CONFIG_PATH+"/bashrc"
 INVALID_ENV_FILE        = "/etc/spork/is/not/valid/profile.env"
 CUSTOM_MIRRORS_FILE     = USER_CONFIG_PATH+"/mirrors"
 SANDBOX_PIDS_FILE       = "/tmp/sandboxpids.tmp"
-
-# since I didn't know wtf this was, it's used for knowing when CONFIG_PROTECT* can be ignored.
-CONFIG_MEMORY_FILE      = PRIVATE_PATH + "/config"
-
-# wtf is this actually used for!?
-#STICKIES=["KEYWORDS_ACCEPT","USE","CFLAGS","CXXFLAGS","MAKEOPTS","EXTRA_ECONF","EXTRA_EINSTALL","EXTRA_EMAKE"]
 
 #CONFCACHE_FILE          = CACHE_PATH+"/confcache"
 #CONFCACHE_LIST          = CACHE_PATH+"/confcache_files.anydbm"
@@ -74,7 +64,4 @@ RSYNC_BIN               = "/usr/bin/rsync"
 RSYNC_HOST              = "rsync.gentoo.org/gentoo-portage"
 
 CVS_BIN                 = "/usr/bin/cvs"
-
-# find a better place for this...
-EBUILD_PHASES			= "setup unpack compile test install preinst postinst prerm postrm"
 
