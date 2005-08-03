a57f61304ed7fc33bf3710b6bb56eb54396e4136
ferringb <ferringb@ac592a22-f3fe-0310-977e-98394eae9e84>
2005-08-03 00:19:18 +0000

    reworked the visibility filter, now it accepts only one instance of a restriction derivative. Why?  Cause that way you can control if it's and, or, xor, etc, or if just a simple check.  Previously it enforced or (effectively)

--- portage/repository/visibility.py
+++ portage/repository/visibility.py
@@ -6,26 +6,22 @@
 # icky.
 # ~harring
 import prototype, errors
+from portage.restrictions.restriction import base
 
 class filterTree(prototype.tree):
 	"""wrap an existing repository filtering results based upon passed in restrictions."""
 
-	def __init__(self, repo, restrictions, sentinel_val=False):
+	def __init__(self, repo, restriction, sentinel_val=False):
 		self.raw_repo = repo
 		self.sentinel_val = sentinel_val
 		if not isinstance(self.raw_repo, prototype.tree):
 			raise errors.InitializationError("%s is not a repository tree derivative" % str(self.raw_repo))
-		if not isinstance(restrictions, list):
-			restrictions = [restrictions]
-		self._restrictions = restrictions
+		if not isinstance(restriction, base):
+			raise errors.InitializationError("%s is not a restriction" % str(restriction)) 
+		self.restriction = restriction
 
 
 	def itermatch(self, atom):
 		for cpv in self.raw_repo.itermatch(atom):
-			ret = True
-			for r in self._restrictions:
-				if r.match(cpv) == self.sentinel_val:
-					ret = False
-					break
-			if ret:
+			if self.restriction.match(cpv) == self.sentinel_val:
 				yield cpv
