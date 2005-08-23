34f1ac8da6f57fb2356cae989b060f684bd60e3c
genone <genone@ac592a22-f3fe-0310-977e-98394eae9e84>
2005-08-23 01:29:11 +0000

    removing files belonging to rewrite (aka savior) branch

--- portage/ebuild/__init__.py
+++
@@ -1,5 +0,0 @@
-import ebuild_package
-import ebuild_repository
-repository	= ebuild_repository.tree
-package		= ebuild_package.ebuild_package
-package_factory = ebuild_package.ebuild_factory
--- portage/package/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright: 2005 Gentoo Foundation
-# Author(s): Brian Harring (ferringb@gentoo.org)
-# License: GPL2
-# $Header$
-
-import metadata
-import cpv
-import atom
