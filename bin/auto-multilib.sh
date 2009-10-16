d9200b289f93ffb1ca2a6fb51e6614514e0956b0
Thomas Sachau <tommy@gentoo.org>
2009-10-16 18:24:21 +0200

    Silence find output

--- bin/auto-multilib.sh
+++ bin/auto-multilib.sh
@@ -365,7 +365,7 @@ _finalize_abi_install() {
 	fi
 
 	# Create wrapper symlink for *-config files
-	local i= files=( $(find "${D}"usr/bin -type f \( -name '*-config' -o -name '*-config-2' \)) )
+	local i= files=( $(find "${D}"usr/bin -type f \( -name '*-config' -o -name '*-config-2' \) 2>/dev/null) )
 	_debug files ${files}
 	for i in ${files}; do
 		prep_ml_binaries "${i}"
